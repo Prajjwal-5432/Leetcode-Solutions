ea3cf091034f5d4af8bc7c4431d9ed94451c67ed
Prajjwal Sahu <prajjwalsahu5432@gmail.com>
2022-04-02 17:10:12 +0530

    Encode and Decode TinyURL

---
+++ HashMap/Medium/1.Encode and Decode TinyURL.cpp
@@ -0,0 +1,12 @@
+//Link: https://leetcode.com/problems/encode-and-decode-tinyurl/
+
+class Solution {
+public:
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
